d337ebeecf95ef0a8ae1eb4a41826a55c392623c
Максим Высоцкий <q79532925360@yandex.ru>
2022-12-27 23:15:53 +0300

    api_yatube

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from api.views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
-router.register('api/v1/posts', PostViewSet)
-router.register('api/v1/groups', GroupViewSet)
-router.register('api/v1/posts/(?P<post_id>\d+)/comments',
+router.register('api/v1/posts', PostViewSet, basename='posts')
+router.register('api/v1/groups', GroupViewSet, basename='groups')
+router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
 urlpatterns = [
